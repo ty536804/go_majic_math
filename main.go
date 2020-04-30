2e9b703b6d1b890da6af79dd9e2bd8a279680b57
xiaobin88 <283817727@qq.com>
2020-04-30 11:07:30 +0800

    优化

--- main.go
+++ main.go
@@ -11,27 +11,27 @@ import (
 	"time"
 )
 
-func main()  {
+func main() {
 	router := Router.InitRouter()
 
-	srv :=http.Server{
-		Addr:":8000",
-		Handler:router,
+	srv := http.Server{
+		Addr:    ":8000",
+		Handler: router,
 	}
 
 	go func() {
-		// 服务连接
+		// 服务连接 aaaaa
 		if err := srv.ListenAndServe(); err != nil && err != http.ErrServerClosed {
 			log.Fatalf("listen: %s\n", err)
 		}
 	}()
 
 	quit := make(chan os.Signal)
-	signal.Notify(quit,os.Interrupt)
+	signal.Notify(quit, os.Interrupt)
 	<-quit
 	log.Println("Shutdown Server ...")
 
-	ctx,cancel := context.WithTimeout(context.Background(), 5 * time.Second)
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
 
 	defer cancel()
 	if err := srv.Shutdown(ctx); err != nil {
