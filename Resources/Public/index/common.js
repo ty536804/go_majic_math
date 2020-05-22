fb584f0d29f44bbfd05afa8200b31c855b53220a
xiaobin <283817727@qq.com>
2020-05-22 13:53:03 +0800

    js获取导航

--- Resources/Public/index/common.js
+++ Resources/Public/index/common.js
@@ -1,41 +1,57 @@
-$('.btn').on('click',function () {
-
-    if ($('.c-area').val()=="") {
-        layer.tips('姓名不能为空', '.c-area', {
-            tips: [1, '#3595CC'],
-            time: 4000
-        });
-        // layer.alert("姓名不能为空");
-        return false;
-    }
-    if ($('.c-tel').val()=="") {
-        layer.tips('电话不能为空', '.c-tel', {
-            tips: [1, '#3595CC'],
-            time: 4000
-        });
-        return false;
-    }
-    if ($('.c-city').val()=="") {
-        layer.tips('地区不能为空', '.c-city', {
-            tips: [1, '#3595CC'],
-            time: 4000
-        });
-        return false;
-    }
+$(function () {
     $.ajax({
-        type: "POST",
+        type: "GET",
         dataType: "json",
-        url: "/AddMessage",
-        data:$('#myform').serialize(),
+        url: "/getNavList",
         success: function (result) {
-            if (result.code == 200) {
-                layer.alert("留言成功");
+            let _html= "";
+            if (Number(result.code) == 200) {
+                $.each(result.data,function (k,v) {
+                    _html += '<a href="'+v.base_url+'">'+v.name+'</a>';
+                })
+            }
+            $(".links").empty().append(_html)
+        }
+    });
+
+    $('.btn').on('click',function () {
+
+        if ($('.c-area').val()=="") {
+            layer.tips('姓名不能为空', '.c-area', {
+                tips: [1, '#3595CC'],
+                time: 4000
+            });
+            // layer.alert("姓名不能为空");
+            return false;
+        }
+        if ($('.c-tel').val()=="") {
+            layer.tips('电话不能为空', '.c-tel', {
+                tips: [1, '#3595CC'],
+                time: 4000
+            });
+            return false;
+        }
+        if ($('.c-city').val()=="") {
+            layer.tips('地区不能为空', '.c-city', {
+                tips: [1, '#3595CC'],
+                time: 4000
+            });
+            return false;
+        }
+        $.ajax({
+            type: "POST",
+            dataType: "json",
+            url: "/AddMessage",
+            data:$('#myform').serialize(),
+            success: function (result) {
+                if (result.code == 200) {
+                    layer.alert("留言成功");
+                    return false
+                }
+                layer.alert("留言失败");
                 return false
             }
-            layer.alert("留言失败");
-            return false
-        }
+        })
+        return false;
     })
-
-    return false;
 })
