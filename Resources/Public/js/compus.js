2e108b7222bdfcd2cfe28b52b1a9942e485b7280
xiaobin <283817727@qq.com>
2020-06-04 09:13:11 +0800

    js

--- Resources/Public/js/compus.js
+++ Resources/Public/js/compus.js
@@ -147,28 +147,26 @@
 
 //分页
     function pageList(page,PageCount) {
-        if (PageCount > 0){
-            $('#pageLimit').bootstrapPaginator({
-                currentPage: page,//当前页。
-                totalPages: PageCount,//总页数。
-                size:"normal",//应该是页眉的大小。
-                bootstrapMajorVersion: 3,//bootstrap的版本要求。
-                alignment:"right",
-                numberOfPages:5,//显示的页数
-                itemTexts: function (type, page, current) {//如下的代码是将页眉显示的中文显示我们自定义的中文。
-                    switch (type) {
-                        case "first": return "首页";
-                        case "prev": return "上一页";
-                        case "next": return "下一页";
-                        case "last": return "末页";
-                        case "page": return page;
-                    }
-                },
-                onPageClicked: function (event, originalEvent, type, page) {//给每个页眉绑定一个事件，其实就是ajax请求，其中page变量为当前点击的页上的数字。
-                    getAjax(page)
+        $('#pageLimit').bootstrapPaginator({
+            currentPage: page,//当前页。
+            totalPages: PageCount,//总页数。
+            size:"normal",//应该是页眉的大小。
+            bootstrapMajorVersion: 3,//bootstrap的版本要求。
+            alignment:"right",
+            numberOfPages:5,//显示的页数
+            itemTexts: function (type, page, current) {//如下的代码是将页眉显示的中文显示我们自定义的中文。
+                switch (type) {
+                    case "first": return "首页";
+                    case "prev": return "上一页";
+                    case "next": return "下一页";
+                    case "last": return "末页";
+                    case "page": return page;
                 }
-            });
-        }
+            },
+            onPageClicked: function (event, originalEvent, type, page) {//给每个页眉绑定一个事件，其实就是ajax请求，其中page变量为当前点击的页上的数字。
+                getAjax(page)
+            }
+        });
     }
 
 
