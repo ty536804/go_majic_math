bba5955430cdb42215416627592e15e58bec4cdb
xiaobin <283817727@qq.com>
2020-06-24 16:18:08 +0800

    js

--- Resources/Public/wap/js/join.js
+++ Resources/Public/wap/js/join.js
@@ -2,21 +2,21 @@ $('.c_com').val(window.location.href);
 
 $('.s_btn').on('click',function () {
     if ($('.banner_con_right .c-area').val()=="") {
-        layer.tips('姓名不能为空', '.footer_con_right .c-area', {
+        layer.tips('姓名不能为空', '.banner_con_right .c-area', {
             tips: [1, '#3595CC'],
             time: 4000
         });
         return false;
     }
     if ($('.banner_con_right .c-tel').val()=="") {
-        layer.tips('电话不能为空', '.footer_con_right .c-tel', {
+        layer.tips('电话不能为空', '.banner_con_right .c-tel', {
             tips: [1, '#3595CC'],
             time: 4000
         });
         return false;
     }
     if ($('.banner_con_right .c-city').val()=="") {
-        layer.tips('地区不能为空', '.footer_con_right .c-city', {
+        layer.tips('地区不能为空', '.banner_con_right .c-city', {
             tips: [1, '#3595CC'],
             time: 4000
         });
