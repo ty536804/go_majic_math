7101e5323f0e880f9157f91355d285f8d2de1ad9
xiaobin <283817727@qq.com>
2020-07-01 14:09:44 +0800

    sms

--- Services/SmsService.go
+++ Services/SmsService.go
@@ -18,13 +18,18 @@ func GetMd5String(s string) string {
 	return hex.EncodeToString(h.Sum(nil))
 }
 
+var (
+	_account  = "C49793087"                        //用户名是登录用户中心->国际短信->产品总览->APIID
+	_password = "bf195a6ea1db1390bee3ad4832b849ea" //查看密码请登录用户中心->国际短信->产品总览->APIKEY
+	_now      = strconv.FormatInt(time.Now().Unix(), 10)
+	_url      = "https://106.ihuyi.com/webservice/sms.php?method=Submit&format=json"
+)
+
 // @Summer 发送验证码
 func SendSms(mobile, msg string) {
 	v := url.Values{}
-	_now := strconv.FormatInt(time.Now().Unix(), 10)
 	//fmt.Printf(_now)
-	_account := "C49793087"                         //用户名是登录用户中心->国际短信->产品总览->APIID
-	_password := "bf195a6ea1db1390bee3ad4832b849ea" //查看密码请登录用户中心->国际短信->产品总览->APIKEY
+
 	v.Set("account", _account)
 	v.Set("password", GetMd5String(_account+_password+mobile+msg+_now))
 	v.Set("mobile", mobile)
@@ -32,15 +37,15 @@ func SendSms(mobile, msg string) {
 	v.Set("time", _now)
 	body := ioutil.NopCloser(strings.NewReader(v.Encode())) //把form数据编下码
 	client := &http.Client{}
-	req, _ := http.NewRequest("POST", "https://106.ihuyi.com/webservice/sms.php?method=Submit&format=json", body)
+	req, _ := http.NewRequest("POST", _url, body)
 
 	req.Header.Set("Content-Type", "application/x-www-form-urlencoded; param=value")
-	fmt.Printf("看下发送的结构 %+v\n", req) //看下发送的结构
+	//fmt.Printf("看下发送的结构 %+v\n", req) //看下发送的结构
 
 	resp, err := client.Do(req) //发送
 	defer resp.Body.Close()     //一定要关闭resp.Body
 	data, _ := ioutil.ReadAll(resp.Body)
-	fmt.Println(string(data), err, "sssss")
+	fmt.Println(string(data), err)
 }
 
 func SendSmsToClient(area, name, tel string) {
