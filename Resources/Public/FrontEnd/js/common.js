4236b3f6866d5851d18cef98f7ab505e11786dac
cxb1988 <283817727@qq.com>
2021-01-13 16:31:22 +0800

    ...

--- Resources/Public/FrontEnd/js/common.js
+++ Resources/Public/FrontEnd/js/common.js
@@ -1,28 +1,34 @@
 $(function () {
     $('.c-com').val(window.location.href);
     $('.bottom_submit').on('click',function () {
-        if ($('#myform .footer_name').val() == "") {
-            layer.tips('姓名不能为空', '.footer_name', {
+        var reg =/[^u4e00-u9fa5]/
+        let name = $.trim($('#myform .footer_name').val())
+        if (name== "" || !reg.test(name)) {
+            layer.tips('姓名不能为空', '#myform .footer_name', {
                 tips: [1, '#3595CC'],
                 time: 4000
             });
             return false;
         }
-        if ($('#myform .footer_tel').val()=="") {
-            layer.tips('电话号码不能为空┖', '.header_from .footer_tel', {
+        // 验证手机号
+        var pattern = /^1\d{10}$/;
+        let phone = $.trim($('#myform .footer_tel').val())
+        if (phone == "" ||  !pattern.test(phone)) {
+            layer.tips('电话号码不能为空', '#myform .footer_tel', {
                 tips: [1, '#3595CC'],
                 time: 4000
             });
             return false;
         }
-        if ($('#myform .city').val()=="") {
-            layer.tips('地区不能为空', '.header_from .footer_city', {
+        let cityName = $.trim($('#myform .footer_city').val())
+        if (cityName ==""  || !reg.test(cityName)) {
+            layer.tips('地区不能为空', '#myform .footer_city', {
                 tips: [1, '#3595CC'],
                 time: 4000
             });
             return false;
         }
-
+        console.log(111)
         $.ajax({
             type: "POST",
             dataType: "json",
