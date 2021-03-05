5d7e6ad4169933b3d1380d007e122fb9741751ba
cxb1988 <283817727@qq.com>
2021-03-05 16:12:06 +0800

    ...

--- Backend/Controller/Message/MessageController.go
+++ Backend/Controller/Message/MessageController.go
@@ -21,6 +21,11 @@ func ListData(c *gin.Context) {
 
 // @Summer 添加留言
 func AddMessage(c *gin.Context) {
+	c.Header("Access-Control-Allow-Origin", "*")
+	c.Header("Access-Control-Allow-Headers", "Content-Type,AccessToken,X-CSRF-Token, Authorization, Token")
+	c.Header("Access-Control-Allow-Methods", "POST, GET, OPTIONS")
+	c.Header("Access-Control-Expose-Headers", "Content-Length, Access-Control-Allow-Origin, Access-Control-Allow-Headers, Content-Type")
+	c.Header("Access-Control-Allow-Credentials", "true")
 	_, msg := Services.AddMessage(c)
 	e.Success(c, msg, "")
 }
